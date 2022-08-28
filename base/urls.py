3540ccedd2af0f8fb2dc130afc295740574218c2
alifirhas <irhasalif@gmail.com>
2022-08-28 20:45:51 +0700

    :triangular_flag_on_post: Interface: user panel

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import overview_view, user_view
 
 urlpatterns = [
-    path("", overview_view.index, name="index"),
-    path("/user/<int:id>", user_view.index, name="user"),
+    path("", overview_view.index, name="admin_index"),
+    
+    path("/user", user_view.index, name="user_index"),
+    path("/user/add", user_view.add, name="user_add"),
+    path("/user/detail", user_view.detail, name="user_detail"),
+    path("/user/detail/menu", user_view.detailRiwayat, name="user_detail_menu"),
 ]
--- base/views/user_view.py
+++ base/views/user_view.py
@@ -3,10 +3,21 @@ from django.http import HttpResponse
 from base.models import User
 
 # Create your views here.
-def index(response, id):
-    user = User.objects.get(id=id)
+def index(response):
     
-    return render(response, "base/admin_user_home.html", {"user": user})
+    return render(response, "base/admin_user.html")
+
+def add(response):
+    
+    return render(response, "base/admin_user_add.html")
+
+def detail(response):
+    
+    return render(response, "base/admin_user_detail.html")
+
+def detailRiwayat(response):
+    
+    return render(response, "base/admin_user_detail_riwayat_menu.html")
 
 def getUser(response, id):
     user = User.objects.get(id=id)
