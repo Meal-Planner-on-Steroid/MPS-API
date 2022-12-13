57365885a2feba93270c3072e1282163927eb68d
alifirhas <irhasalif@gmail.com>
2022-12-13 21:53:41 +0700

    :children_crossing: Tampil detail hasil algoritma genetika

--- base/views/generate_rekomendasi_menu_view.py
+++ base/views/generate_rekomendasi_menu_view.py
@@ -44,31 +44,88 @@ def hasil(response):
         generasi = ag.generateGenerasi(blok_makanan=list_blok_makanan)
         rekomendasi = generasi
         
-        for row in range(0,20):
-            nilai_fitness = ag.hitungNilaiFitness(generasi, kebutuhan_gizi)
-            crossover = ag.selectCross(nilai_fitness['set_nilai_fitness'], copy.deepcopy(generasi))
+        i = 0
+            
+        # for row in range(0,100):
+        while True:
+            nilai_fitness = ag.hitungNilaiFitness(rekomendasi, kebutuhan_gizi)
+            avg_fitness = ag.avgNilaiFitnessGizi(copy.deepcopy(nilai_fitness['set_nilai_fitness']))
+            
+            crossover = ag.selectCross(copy.deepcopy(nilai_fitness['set_nilai_fitness']), copy.deepcopy(rekomendasi))
             generasi_mutasi = ag.mutasi(copy.deepcopy(crossover['childs']), list_blok_makanan)
-            generasi_elit = ag.elitism(copy.deepcopy(generasi), nilai_fitness['set_nilai_fitness'])
+            generasi_elit = ag.elitism(copy.deepcopy(rekomendasi), nilai_fitness['set_nilai_fitness'])
             populasi_baru = ag.bentukPopulasiBaru(generasi_mutasi, generasi_elit)
             
             if with_detail:
                 detail_gen.append({
                     'generasi': rekomendasi,
                     'nilai_fitness': nilai_fitness,
+                    'avg_fitness': avg_fitness,
                     'crossover': crossover,
                     'generasi_mutasi': generasi_mutasi,
                     'generasi_elit': generasi_elit,
                 })
+                
+            rekomendasi = copy.deepcopy(populasi_baru)
             
-            rekomendasi = populasi_baru
+            if i % 10 == 0:
+                print('================')
+                print(avg_fitness)
+                print('Ada di: ',i)         
+            
+            if i >= 50:
+                print('================ ', i)
+                print(avg_fitness)
+                print('break di i')
+                break
+            
+            if avg_fitness['protein'] > 0.5 and avg_fitness['lemak'] > 0.5 and avg_fitness['karbo'] > 0.5:
+                print('================', i)
+                print(avg_fitness)
+                print('break di avg')
+                break
+            
+            i += 1
         
         result = {
             'kebutuhan_gizi': kebutuhan_gizi,
             'rekomendasi': rekomendasi[:7],
+            'haris': [
+                {
+                    "id": 0,
+                    "name": 'Senin',
+                },
+                {
+                    "id": 1,
+                    "name": 'Selasa',
+                },
+                {
+                    "id": 2,
+                    "name": 'Rabu',
+                },
+                {
+                    "id": 3,
+                    "name": 'Kamis',
+                },
+                {
+                    "id": 4,
+                    "name": 'Jumat',
+                },
+                {
+                    "id": 5,
+                    "name": 'Sabtu',
+                },
+                {
+                    "id": 6,
+                    "name": 'Minggu',
+                },
+            ],
         }
         
         if with_detail:
-            result['detail_proses'] = detail_gen
+            result['detail_proses_start'] = detail_gen[0]
+            result['detail_proses_mid'] = detail_gen[round(i/2)]
+            result['detail_proses_end'] = detail_gen[i-1]
         
         # return HttpResponse(result['kebutuhan_gizi']['amb']);
         return render(response, "base/generate_rekomendasi_menu_hasil.html", result) 
